4ce1c5ba08d80c2097a06dac0045e37f873d0914
WinileMbatha <mashanduwn@gmail.com>
2023-06-15 14:14:25 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#includes <stdio.h>
+/**
+ * main - Printing a line with put funtion using c programing"
+ *
+ *Return:0 (Success)
+ */
+int main(void)
+[
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
